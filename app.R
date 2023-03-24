de6912b849cd94886f83cd6b04e8724da78a2fe4
hcwang24 <hcwang24@student.ubc.ca>
2023-03-23 17:10:22 -0700

    Adding upload button and table is now a df

--- app.R
+++ app.R
@@ -1,3 +1,7 @@
+# Solved package not found message with runing the code below in console.
+# library(BiocManager)
+# options(repos = BiocManager::repositories())
+
 library(shiny)
 library(edgeR)
 library(tidyverse)
@@ -5,49 +9,67 @@ library(tidyverse)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Gene Differential Expression Explorer"),
+  # Application title
+  titlePanel("Gene Differential Expression Explorer"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          # Sidebar panel for file upload
-          fileInput("file1", 
-                    "Upload Metadata .csv File (containing file names and grouping information",
-                    multiple = FALSE,
-                    accept = c("text/csv",
-                               "text/comma-separated-values,text/plain",
-                               ".csv")),
-          
-          # Input: Select separator ----
-          radioButtons("sep", "Separator",
-                       choices = c(Comma = ",",
-                                   Semicolon = ";",
-                                   Tab = "\t"),
-                       selected = ","),
-          
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           tableOutput("metadata_contents")
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      # Sidebar panel for file upload
+      fileInput("file1",
+        "Upload metadata.csv File (containing list of files with grouping information",
+        multiple = FALSE,
+        accept = c(
+          "text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv"
         )
+      ),
+
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+        choices = c(
+          Comma = ",",
+          Semicolon = ";",
+          Tab = "\t"
+        ),
+        selected = ","
+      ),
+      
+      actionButton("upload", "Upload"),
+      
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      h3("Exploratory Data Analysis"),
+      tableOutput("metadata_contents"),
+      textOutput("grouping")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$metadata_contents <- renderTable({
-      req(input$file1)
-      
-      df <- read_csv(
-        input$file1$datapath,
-        col_names = TRUE,
-      )
-      head(df)
-    })
+  # Loading metadata table
+  metadata_df <- eventReactive(input$upload, {
+    req(input$file1)
+    read_delim(
+      input$file1$datapath,
+      delim = input$sep,
+      col_names = TRUE,
+      show_col_types = FALSE
+    )
+  })
+  
+  output$metadata_contents <- renderTable({
+    metadata_df()
+  })
+  
+  output$grouping <- renderText({
+    print(levels(metadata_df()$group))
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
